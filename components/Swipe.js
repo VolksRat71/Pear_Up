2bda501848d74592a5e464bc03b20b2a31612780
VolksRat71 <nathanieljryan1994@gmail.com>
2019-08-27 15:38:27 -0700

    Swipe.js component created

---
+++ components/Swipe.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+class Swipe extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hey, there!</Text>
+            </View>
+        );
+    }
+}
+
+export default Swipe;
