b689c1eda3de4be85db0e4a7764e78ebec448d68
VolksRat71 <nathanieljryan1994@gmail.com>
2019-08-27 15:35:42 -0700

    Profile.js data created with hard coded profile information

---
+++ components/Profiles.js
@@ -0,0 +1,36 @@
+export default [{
+        "name": "Nate Ryan",
+        "city": "Portland, OR",
+        "schedule": "06:30p - 09:00p",
+        "typeOfWorkout": "Calisthenics",
+        "music": "The Growlers"
+    },
+    {
+        "name": "Blaine Howitt",
+        "city": "Portland, OR",
+        "schedule": "06:30p - 09:00p",
+        "typeOfWorkout": "Calisthenics",
+        "music": "The Growlers"
+    },
+    {
+        "name": "Arlen Cornejo",
+        "city": "Portland, OR",
+        "schedule": "06:30p - 09:00p",
+        "typeOfWorkout": "Calisthenics",
+        "music": "The Growlers"
+    },
+    {
+        "name": "Herbert Camposa",
+        "city": "Portland, OR",
+        "schedule": "06:30p - 09:00p",
+        "typeOfWorkout": "Calisthenics",
+        "music": "The Growlers"
+    },
+    {
+        "name": "Terence Oddie",
+        "city": "Portland, OR",
+        "schedule": "06:30p - 09:00p",
+        "typeOfWorkout": "Calisthenics",
+        "music": "The Growlers"
+    }
+]
