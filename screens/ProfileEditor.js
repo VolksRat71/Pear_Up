5ac0adc664d59310ab8736ee5b2e1c3f33dacf9d
VolksRat71 <nathanieljryan1994@gmail.com>
2019-09-19 15:14:52 -0700

    avatar button successfully moved

--- screens/ProfileEditor.js
+++ screens/ProfileEditor.js
@@ -1,16 +1,81 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import NavBar from '../components/NavBar'
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import {
+    StyleSheet, View,
+    Button, ImageEditor,
+} from 'react-native';
+import FirebaseSDK from '../config/FirebaseSDK';
 
-const ProfileEditor = props => {
-    return (
-        <View style={styles.viewPort}>
-            <NavBar />
-            <View style={styles.profileEditorScreen}>
-                <Text>Profile Editor</Text>
+class ProfileEditor extends React.Component {
+
+    onImageUpload = async () => {
+        const { status: cameraRollPerm } = await Permissions.askAsync(
+            Permissions.CAMERA_ROLL
+        );
+        try {
+            // only if user allows permission to camera roll
+            if (cameraRollPerm === 'granted') {
+                console.log('choosing image granted...');
+                let pickerResult = await ImagePicker.launchImageLibraryAsync({
+                    allowsEditing: true,
+                    aspect: [4, 3],
+                });
+                console.log(
+                    'ready to upload... pickerResult json:' + JSON.stringify(pickerResult)
+                );
+
+                var wantedMaxSize = 150;
+                var rawheight = pickerResult.height;
+                var rawwidth = pickerResult.width;
+
+                var ratio = rawwidth / rawheight;
+                var wantedwidth = wantedMaxSize;
+                var wantedheight = wantedMaxSize / ratio;
+                // check vertical or horizontal
+                if (rawheight > rawwidth) {
+                    wantedwidth = wantedMaxSize * ratio;
+                    wantedheight = wantedMaxSize;
+                }
+                console.log("scale image to x:" + wantedwidth + " y:" + wantedheight);
+                let resizedUri = await new Promise((resolve, reject) => {
+                    ImageEditor.cropImage(pickerResult.uri,
+                        {
+                            offset: { x: 0, y: 0 },
+                            size: { width: pickerResult.width, height: pickerResult.height },
+                            displaySize: { width: wantedwidth, height: wantedheight },
+                            resizeMode: 'contain',
+                        },
+                        (uri) => resolve(uri),
+                        () => reject(),
+                    );
+                });
+                let uploadUrl = await FirebaseSDK.uploadImage(resizedUri);
+                //let uploadUrl = await FirebaseSDK.uploadImageAsync(resizedUri);
+                await this.setState({ avatar: uploadUrl });
+                console.log(" - await upload successful url:" + uploadUrl);
+                console.log(" - await upload successful avatar state:" + this.state.avatar);
+                await FirebaseSDK.updateAvatar(uploadUrl); //might failed
+            }
+        } catch (err) {
+            console.log('onImageUpload error:' + err.message);
+            alert('Upload image error:' + err.message);
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.viewPort}>
+                <View style={styles.profileEditorScreen}>
+                    <Button
+                        title="Upload Avatar Image"
+                        style={styles.buttonText}
+                        onPress={this.onImageUpload}
+                    />
+                </View>
             </View>
-        </View>
-    )
+        )
+    }
 }
 
 const styles = StyleSheet.create({
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import * as Permissions from 'expo-permissions';
-import * as ImagePicker from 'expo-image-picker';
 import {
     StyleSheet, Text,
     TextInput, View,
-    Button, ImageEditor,
+    Button,
 } from 'react-native';
 import FirebaseSDK from '../config/FirebaseSDK';
 
@@ -36,60 +34,6 @@ class CreateAccount extends React.Component {
     onChangeTextPassword = password => this.setState({ password });
     onChangeTextName = name => this.setState({ name });
 
-    onImageUpload = async () => {
-        const { status: cameraRollPerm } = await Permissions.askAsync(
-            Permissions.CAMERA_ROLL
-        );
-        try {
-            // only if user allows permission to camera roll
-            if (cameraRollPerm === 'granted') {
-                console.log('choosing image granted...');
-                let pickerResult = await ImagePicker.launchImageLibraryAsync({
-                    allowsEditing: true,
-                    aspect: [4, 3],
-                });
-                console.log(
-                    'ready to upload... pickerResult json:' + JSON.stringify(pickerResult)
-                );
-
-                var wantedMaxSize = 150;
-                var rawheight = pickerResult.height;
-                var rawwidth = pickerResult.width;
-
-                var ratio = rawwidth / rawheight;
-                var wantedwidth = wantedMaxSize;
-                var wantedheight = wantedMaxSize / ratio;
-                // check vertical or horizontal
-                if (rawheight > rawwidth) {
-                    wantedwidth = wantedMaxSize * ratio;
-                    wantedheight = wantedMaxSize;
-                }
-                console.log("scale image to x:" + wantedwidth + " y:" + wantedheight);
-                let resizedUri = await new Promise((resolve, reject) => {
-                    ImageEditor.cropImage(pickerResult.uri,
-                        {
-                            offset: { x: 0, y: 0 },
-                            size: { width: pickerResult.width, height: pickerResult.height },
-                            displaySize: { width: wantedwidth, height: wantedheight },
-                            resizeMode: 'contain',
-                        },
-                        (uri) => resolve(uri),
-                        () => reject(),
-                    );
-                });
-                let uploadUrl = await FirebaseSDK.uploadImage(resizedUri);
-                //let uploadUrl = await FirebaseSDK.uploadImageAsync(resizedUri);
-                await this.setState({ avatar: uploadUrl });
-                console.log(" - await upload successful url:" + uploadUrl);
-                console.log(" - await upload successful avatar state:" + this.state.avatar);
-                await FirebaseSDK.updateAvatar(uploadUrl); //might failed
-            }
-        } catch (err) {
-            console.log('onImageUpload error:' + err.message);
-            alert('Upload image error:' + err.message);
-        }
-    };
-
     render() {
         return (
             <View>
@@ -117,11 +61,6 @@ class CreateAccount extends React.Component {
                     title="Create Account"
                     style={styles.buttonText}
                     onPress={this.onPressCreate}
-                />
-                <Button
-                    title="Upload Avatar Image"
-                    style={styles.buttonText}
-                    onPress={this.onImageUpload}
                 />
             </View>
         );
